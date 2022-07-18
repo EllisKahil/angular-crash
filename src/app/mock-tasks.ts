30c542ad702eeb4e0c0165e5e71aa2d5875963c2
EllisKahil <86572824+EllisKahil@users.noreply.github.com>
2022-07-17 22:20:50 -0400

    Angular Assignment

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Manicure/Pedicure',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Massage Theraphy',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Grocery Shopping',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+];
